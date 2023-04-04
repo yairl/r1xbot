aa1355d0e723bb8b5a59e9681912cd913da21a5e
Yair Lifshitz <yair@lifshitz.io>
2023-04-04 03:41:52 +0300

    1. messenger::sendMessage support. 2. Introduce senderIsMe field.

---
+++ handleMessages/src/services/messengers/tg.js
@@ -0,0 +1,54 @@
+function parseMessage(message) {
+  message = message.message;
+
+  const source = "tg";
+  const messageTimestamp = message.date;
+  const chatId = message.chat.id.toString();
+  const senderId = message.from.id.toString();
+  const senderIsMe = (message.from.id == process.env.TELEGRAM_SENDER_ID);
+  const messageId = message.message_id.toString();
+  const kind = "text";
+  const body = message.text;
+
+  return {
+    source,
+    messageTimestamp,
+    chatId,
+    senderId,
+    senderIsMe,
+    messageId,
+    kind,
+    body,
+    rawSource: message
+  };
+}
+
+async function sendMessage(attributes) {
+  const {
+    chatId,
+    quoteId,
+    kind,
+    body
+  } = attributes;
+
+  if (kind != 'text') {
+    return ;
+  }
+
+  const axios = require('axios');
+
+  var args = { chat_id : chatId, text : body };
+  if (quoteId) {
+      args.reply_to_message_id = quoteId;
+  }
+
+  const response = await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, args);
+  console.log(response);
+}
+
+
+module.exports = {
+    parseMessage,
+    sendMessage
+};
+
