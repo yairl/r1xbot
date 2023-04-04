e89f5429500b677a14c68446f81c7585ec4d2d6d
Yair Lifshitz <yair@lifshitz.io>
2023-04-04 03:41:52 +0300

    1. messenger::sendMessage support. 2. Introduce senderIsMe field.

--- handleMessages/src/controllers/handle-incoming-messages.js
+++ handleMessages/src/controllers/handle-incoming-messages.js
@@ -11,7 +11,10 @@ const { insertMessage } = require("../services/messages/messages-service");
 
 async function handleIncomingMessage(event) {
   try {
-    const parsedEvent = parseSqsEvent(event);
+    const parsedEvent = JSON.parse(event.Records[0].body);
+
+    const messenger = require("../services/messengers/" + parsedEvent.source);
+    const parsedMessage = messenger.parseMessage(parsedEvent.event);
 
     const message = await insertMessage(parsedEvent);
     //console.log(message.toJSON());
@@ -21,64 +24,6 @@ async function handleIncomingMessage(event) {
   } catch (error) {
     console.log(error.stack);
   }
-}
-
-function parseSqsEvent(event) {
-  const eventDataStringified = event.Records[0].body;
-
-  parsedEvent = JSON.parse(eventDataStringified);
-
-  switch (parsedEvent.source) {
-    case "tg":
-      return parseTelegramMessage(parsedEvent.event);
-    case "wa":
-      return parseWhatsappMessage(parsedEvent.event);
-  }
-}
-
-function parseTelegramMessage(message) {
-  message = message.message;
-
-  const source = "tg";
-  const messageTimestamp = message.date;
-  const chatId = message.chat.id.toString();
-  const senderId = message.from.id.toString();
-  const messageId = message.message_id.toString();
-  const kind = "text";
-  const body = message.text;
-
-  return {
-    source,
-    messageTimestamp,
-    chatId,
-    senderId,
-    messageId,
-    kind,
-    body,
-    rawSource: message
-  };
-}
-
-function parseWhatsappMessage(message) {
-  const source = "wa";
-  const messageTimestamp = message.data.time;
-  const chatId = message.data.from;
-  const senderId =
-    message.data.author == "" ? message.data.from : message.data.author;
-  const messageId = message.data.id;
-  const kind = "text";
-  const body = message.data.body;
-
-  return {
-    source,
-    messageTimestamp,
-    chatId,
-    senderId,
-    messageId,
-    kind,
-    body,
-    rawSource: message
-  };
 }
 
 module.exports = {
---
+++ handleMessages/src/services/messengers/wa.js
@@ -0,0 +1,55 @@
+function parseMessage(message) {
+  myId = ```${process.env.R1X_NUMBER}@c.us```;
+
+  const source = "wa";
+  const messageTimestamp = message.data.time;
+  const chatId = message.data.from;
+  const senderId =
+    message.data.author == "" ? message.data.from : message.data.author;
+  const senderIsMe = message.data.fromMe;
+  const messageId = message.data.id;
+  const kind = "text";
+  const body = message.data.body;
+
+  return {
+    source,
+    messageTimestamp,
+    chatId,
+    senderId,
+    senderIsMe,
+    messageId,
+    kind,
+    body,
+    rawSource: message
+  };
+}
+
+async function sendMessage(attributes) {
+  const {
+    chatId,
+    quoteId,
+    kind,
+    body
+  } = attributes;
+
+  if (kind != 'text') {
+    return ;
+  }
+
+  const axios = require('axios');
+
+  var args = { token : process.env.WHATSAPP_TOKEN, to : chatId, body : body };
+  
+  if (quoteId) {
+      args.msgId = quoteId;
+  }
+
+  const response = await axios.post(`https://api.ultramsg.com/instance${process.env.WHATSAPP_INSTANCE}/messages/chat`, args);
+  console.log(response);
+}
+
+module.exports = {
+  parseMessage,
+  sendMessage
+};
+
