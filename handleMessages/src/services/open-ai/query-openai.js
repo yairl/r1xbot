79eeecef3d2c3723affb49fa2fe3b63d68e682ad
Yair Lifshitz <yair@lifshitz.io>
2023-04-05 15:29:38 +0300

    Do not reply to unsupported message.

--- handleMessages/src/services/open-ai/query-openai.js
+++ handleMessages/src/services/open-ai/query-openai.js
@@ -16,18 +16,29 @@ async function getChatCompletion(ctx, messages) {
   const parsedMessages = [
     {
       role: "system",
-      content: `You are a helpful expert assistant, Robot 1-X, integrated into a WhatsApp chat. Today is ${new Date(
+      content: `You are a helpful expert assistant, Robot 1-X, integrated into a Telegram chat. Today's date is ${new Date(
         Date.now()
       ).toDateString()}. More information about you is available at https://r1x.ai. When telling about yourself, prefer to provide the link as well.`
     }
   ];
+
   for (message of messages) {
+    if (message.body == null) {
+      continue;
+    }
+ 
     parsedMessages.push(convertMessageToChatFormat(message));
   }
+
+  console.log(`[${ctx}] getChatCompletion messages: `, parsedMessages); 
+
   const completion = await openai.createChatCompletion({
     model: process.env.OPENAI_MODEL,
     messages: parsedMessages
   });
+
+  console.log(`[${ctx}] getChatCompletion response: `, completion);
+
   return completion.data.choices[0].message.content;
 }
 
