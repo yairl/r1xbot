c3ef13e62bd440da548a0b47a18f2d9287c28e7f
Eli Siegel <elisi@monday.com>
2023-04-06 07:46:33 +0200

    parallel polling - not checked

--- handleMessages/process-sqs.js
+++ handleMessages/process-sqs.js
@@ -5,30 +5,36 @@ const { handler } = require("./index");
 
 const ctx = { msgCount: 0 };
 
-const app = Consumer.create({
-  queueUrl: process.env.SQS_QUEUE_URL,
-  handleMessage: async (message) => {
-    currMsgCount = ++ctx.msgCount;
-    console.log(`[${currMsgCount}] `, "Starting to handle message");
+const numOfConsumers = 10;
+const consumers = [];
 
-    const result = await handler(currMsgCount, message.Body);
-    console.log(`[${currMsgCount}] `, "Finished handling message");
-  },
-  sqs: new SQSClient({
-    region: "eu-central-1"
-  })
-});
+for (let i = 0; i < numOfConsumers; i++) {
+  const app = Consumer.create({
+    queueUrl: process.env.SQS_QUEUE_URL,
+    handleMessage: async (message) => {
+      currMsgCount = ++ctx.msgCount;
+      console.log(`[${currMsgCount}] `, "Starting to handle message");
 
-app.on("error", (err) => {
-  console.error(err.message);
-});
+      const result = await handler(currMsgCount, message.Body);
+      console.log(`[${currMsgCount}] `, "Finished handling message");
+    },
+    sqs: new SQSClient({
+      region: "eu-central-1"
+    })
+  });
 
-app.on("processing_error", (err) => {
-  console.error(err.message);
-});
+  app.on("error", (err) => {
+    console.error(err.message);
+  });
 
-app.on("timeout_error", (err) => {
-  console.error(err.message);
-});
+  app.on("processing_error", (err) => {
+    console.error(err.message);
+  });
 
-app.start();
+  app.on("timeout_error", (err) => {
+    console.error(err.message);
+  });
+
+  app.start();
+  consumers.push(app);
+}
