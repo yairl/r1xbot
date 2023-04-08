96969da80dd8a7be30f59d7381fd8153c815209d
Igor Shumsky <ig.shum@gmail.com>
2023-04-08 05:40:21 +0300

    - Added token predition capability, with filtering for last message upto maxToken - Test for token prediction

---
+++ handleMessages/test/test-token-predictor.js
@@ -0,0 +1,80 @@
+"use strict";
+const assert = require('assert');
+require("dotenv").config();
+const tokenPredictor = require(".././src/services/token-prediction/token-predictor");
+
+async function main() {
+    let systemMessage = {"role": "system", "content": "you are a helpful bot"};
+
+    let chatMessages = [
+         {"role": "user", "content": "i'm your lord"},
+         {"role": "assistant", "content": "you'd wish"},
+         {"role": "user", "content": "מה אתה אומר"},
+         {"role": "assistant", "content": "מה שאתה שומע"}
+    ];
+
+    // build the expected behavior. a map from number of tokens --> message list using the numTokensFromMessages predictor
+    let numTokensToMessages = new Map();
+    let previousMessages = [];
+    let previousNumTokens = 0;
+    const chatMessagesReversed = chatMessages.slice().reverse(); // make a copy of chatMessages and reverse it
+    
+    for (let numChatMessages = 0; numChatMessages <= chatMessagesReversed.length ; ++numChatMessages) {
+        // take the first numChatMessages from the end
+        let subsetChatMessagesReversed = chatMessagesReversed.slice(0, numChatMessages);
+
+        // get back the normal order 
+        let subsetChatMessages = subsetChatMessagesReversed.slice().reverse();
+
+        // build the full list message
+        let combinedMessages = [systemMessage, ...subsetChatMessages];
+
+        // calculate their number of tokens
+        let numTokens = await tokenPredictor.numTokensFromMessages(combinedMessages);
+
+        // fill out the entries of token sizes for the unset sizes so far in the message map
+        for (let token = previousNumTokens; token < numTokens; token++) {
+            numTokensToMessages[token] = previousMessages;
+        }
+
+        // if this is the last iteration then nothing will fill in for it (no next), so do now
+        if (numChatMessages == chatMessagesReversed.length) {
+            
+            numTokensToMessages[numTokens] = combinedMessages;
+        }
+
+        // update for next iteration
+        previousMessages = combinedMessages;
+        previousNumTokens = numTokens;
+    }
+    // console.log(numTokensToMessages);
+     
+      
+    // verify that the message selector picks up exactly the expected messages. Go +10 beyond the last previousNumTokens just for checks.
+    for (let maxTokens = 0; maxTokens < previousNumTokens+10; maxTokens ++) {
+        let actualMessages = await tokenPredictor.getMessagesUptoMaxTokens(systemMessage, chatMessages, maxTokens);
+
+        //console.log(`maxTokens=${maxTokens}\n  actual=${JSON.stringify(actualMessages)}\nexpected=${JSON.stringify(numTokensToMessages[maxTokens])}`);
+        try {
+            // if the number of tokens still exist int the map, compare to the map
+            if (maxTokens <= previousNumTokens){
+                assert.deepStrictEqual(actualMessages, numTokensToMessages[maxTokens]);
+            } else {
+                //compare the the full list
+                assert.deepStrictEqual(actualMessages, previousMessages);
+            }
+        } catch (error) {
+            console.log(`Actual and expected message list are different for ${maxTokens}\n`, error);
+            throw error;
+        }
+    }
+    
+    // Get the index of the message within the chatMessages list from which messages should be taken.
+    // let res = await tokenPredictor.getMessageIndexUptoMaxTokens(systemMessage, chatMessages, 17); console.log(res);
+
+    // Get the number of tokens a list of messages should consume
+    // tokens = await tokenPredictor.numTokensFromMessages(combined);  console.log("numTokensFromMessages", tokens);
+}
+
+main();
+
