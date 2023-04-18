66ad23560f37a7354356ce505282c559d0066afb
Yair Lifshitz <yair@lifshitz.io>
2023-04-18 10:20:05 +0300

    tools/stats.py: add CLI arguments.

--- tools/stats.py
+++ tools/stats.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
+import argparse
 import dotenv
 import numpy
 import os
 import psycopg2
 import psycopg2.extras
-
-START_DATE='2023/04/16'
-END_DATE='2023/04/16'
 
 dotenv.load_dotenv('.env.prod')
 ps = psycopg2.connect(os.environ['DB_CONNECTION_STRING'])
@@ -41,10 +39,15 @@ def get_active_chat_histogram(start_date, end_date):
 
     return chats
 
-num_msgs = get_message_count(START_DATE, END_DATE)
+parser = argparse.ArgumentParser(description='Fetch statistics from R1X database.')
+parser.add_argument('--start-date', type=str, dest='start_date', help='Start date.', required=True)
+parser.add_argument('--end-date', type=str, dest='end_date', help='End date.', required=True)
+args = parser.parse_args()
+
+num_msgs = get_message_count(args.start_date, args.end_date)
 print('Number of messages: ', num_msgs)
 
-chats = get_active_chat_histogram(START_DATE, END_DATE)
+chats = get_active_chat_histogram(args.start_date, args.end_date)
 print('Active chats today: ', len(chats))
 
 msg_arr = []
@@ -52,10 +55,10 @@ msg_arr = []
 for chat in chats:
     (chat_id, msgs) = chat
     msg_arr.append(msgs)
-    if msgs < 100:
+    if msgs < 20:
         continue
     
-    #print(chat_id, msgs)
+    print(chat_id, msgs)
 
 print(numpy.histogram(msg_arr, [0, 5, 10, 15, 20, 50, 100]))
 
