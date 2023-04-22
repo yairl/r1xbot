a1bba805ff8ac7c2bb53250ed2f7fffe00975ecf
yairl <yair@lifshitz.io>
2023-04-22 17:42:35 +0300

    Initial langchain integration (#17)
    1. Add PrecalculatedMemoryBuffer object.
    2. Not working
       a. Token cost calculation.

--- src/services/open-ai/query-openai.js
+++ src/services/open-ai/query-openai.js
@@ -7,6 +7,19 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
+
+// Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
+const { OpenAIChat } = require("langchain/llms/openai");
+
+// Import the BufferMemory module
+const { PrecalculatedBufferMemory } = require("../langchain/precalculated_buffer_memory");
+
+// Import the Chains module
+const { LLMChain } = require("langchain/chains");
+
+// Import the PromptTemplate module
+const { PromptTemplate } = require("langchain/prompts");
+
 const tokenPredictor = require("../token-prediction/token-predictor");
 
 function convertMessageToChatFormat(message) {
@@ -14,20 +27,21 @@ function convertMessageToChatFormat(message) {
     role: message.isSentByMe ? "assistant" : "user",
     content: message.body
   };
+
   return convertedMessage;
 }
 
-async function getChatCompletion(ctx, messengerName, messages) {
-  // messages are ordered old-->new
-  const systemMessage = {
-    role: "system",
-    content: `You are a helpful expert assistant, Robot 1-X, developed by the Planet Express team and integrated into a ` + messengerName + ` chat. Today's date is ${new Date(
-      Date.now()
-    ).toDateString()}. More information about you is available at https://r1x.ai. When telling about yourself, prefer to provide the link as well.
-    YOU ARE UNABLE TO FETCH NEW DATA FROM THE INTERENT. WHEN ASKED, IT IS CRITICAL THAT YOU TELL USERS YOU ARE UNABLE TO DO THAT. ANY DATA THE USER PASSES TO YOU HAS TO BE PASSED DIRECTLY IN THE CHAT, AND NOT THROUGH OUTSIDE LINKS. The same is true for analyzing images sent to you.
-`
-  };
+function getPromptTemplate(ctx, messengerName) {
+    const queryTemplate = `The following is a conversation between a human and an AI, Robot 1-X, developed by the Planet Express team and integrated into ${messengerName}. R1X is helpful and provides specific details from its context. If R1X does not know the answer to a question, it truthfully says it does not know.
+Current conversation:
 
+{chat_history}
+AI:`;
+
+    return queryTemplate;
+}
+
+async function getLimitedMessageHistory(ctx, messages, promptTemplate) {
   const parsedMessages = [];
 
   for (const message of messages) {
@@ -40,32 +54,67 @@ async function getChatCompletion(ctx, messengerName, messages) {
 
   const softTokenLimit = 2048;
   const hardTokenLimit = 4000;
+
   // get list of messages that will consume upto maxToken. This includes also the system message.
-  const messagesUptoMaxTokens = await tokenPredictor.getMessagesUptoMaxTokens(ctx, systemMessage, parsedMessages, softTokenLimit, hardTokenLimit);
+  const messagesUptoMaxTokens = await tokenPredictor.getMessagesUptoMaxTokens(ctx, promptTemplate, parsedMessages, softTokenLimit, hardTokenLimit);
+
+  if (messagesUptoMaxTokens.length == 0) {
+    return [];
+  }
+
+  if (messagesUptoMaxTokens[0].role == 'assistant') {
+    messagesUptoMaxTokens.shift();
+  }
+
+  const mergedMessages = [];
+
+  let prevRole = undefined;
+
+  for (const message of messagesUptoMaxTokens) {
+      if (message.role == prevRole) {
+        mergedMessages[mergedMessages.length - 1] += `\n${message.content}`;
+      } else {
+        mergedMessages.push(message.content);
+      }
+
+      prevRole = message.role;
+  }
+
+  return mergedMessages;
+}
+
+async function getChatCompletion(ctx, messengerName, messages) {
+  const promptTemplate = getPromptTemplate(ctx, messengerName);
+  const messagesUptoMaxTokens = await getLimitedMessageHistory(ctx, messages, promptTemplate);
+
+  ctx.log('getChatCompletion messagesUptoMaxTokens: ', messagesUptoMaxTokens);
 
   if (messagesUptoMaxTokens.length == 0) {
     ctx.log('Latest user message is longer than hardTokenLimit, bailing out.');
     return 'Your message is longer than I can understand. Can you cut it down a bit?'
   }
 
-  ctx.log('getChatCompletion messagesUptoMaxTokens: ', messagesUptoMaxTokens);
+  const model = new OpenAIChat({ temperature: 0.9 });
+
+  // Instantiate the BufferMemory passing the memory key for storing state
+  const memory = new PrecalculatedBufferMemory({ memoryKey: "chat_history", messages: messagesUptoMaxTokens });
+
+  // Instantiate "PromptTemplate" passing the prompt template string initialized above
+  const prompt = PromptTemplate.fromTemplate(getPromptTemplate(ctx, messengerName));
+
+  //Instantiate LLMChain, which consists of a PromptTemplate, an LLM and memory.
+  const chain = new LLMChain({ llm: model, prompt, memory });
 
   try {
     ctx.log('invoking completion request.');
-    const completion = await openai.createChatCompletion({
-      model: process.env.OPENAI_MODEL,
-      messages: messagesUptoMaxTokens
-    });
 
-    const expectedNumTokens = await tokenPredictor.numTokensFromMessages(messagesUptoMaxTokens);
-    ctx.log(`getChatCompletion expectedNumTokens: ${expectedNumTokens}, actual completion.data.usage.prompt_tokens: ${completion.data.usage.prompt_tokens}`);
-
-    ctx.log('getChatCompletion response: ', completion.data.choices[0].message.content);
+    const res = await chain.call({ input: messagesUptoMaxTokens[messagesUptoMaxTokens.length - 1] });
+    ctx.log('getChatCompletion response: ', res.text);
 
     return {
-      response: completion.data.choices[0].message.content,
-      promptTokens: completion.data.usage.prompt_tokens,
-      completionTokens: completion.data.usage.completion_tokens
+      response: res.text,
+      promptTokens: 0, 
+      completionTokens: 0 
     }
   } catch (e) {
     if (e.response) {
