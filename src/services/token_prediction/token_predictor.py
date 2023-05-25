0e1b796127a7937a0bcd554fdf663ac74d601d9f
Yair Lifshitz <yair@lifshitz.io>
2023-05-26 00:31:54 +0300

    Refactor message service to handle insertions.

--- src/services/token_prediction/token_predictor.py
+++ src/services/token_prediction/token_predictor.py
@@ -3,13 +3,9 @@ import os
 import tiktoken
 
 # global variable to hold the encode objects between invocations
-encoder = None
+encoder = tiktoken.get_encoding("cl100k_base")
 
-def num_tokens_from_messages(messages):
-    global encoder
-    if not encoder:
-        raise ValueError("encoder is not initialized")
-
+def _num_tokens_from_messages(messages):
     num_tokens = 0
     for message in messages:
         num_tokens += 4
@@ -22,7 +18,7 @@ def num_tokens_from_messages(messages):
     num_tokens += 1
     return num_tokens
 
-def get_message_tokens(message):
+def _get_message_tokens(message):
     if len(message) == 0:
         raise ValueError(f"message is malformed. It's {message} but doesn't have any keys")
 
@@ -35,11 +31,7 @@ def get_message_tokens(message):
 
     return num_tokens
 
-def get_message_index_upto_max_tokens(system_message, chat_messages, soft_token_limit, hard_token_limit):
-    global encoder
-    if not encoder:
-        raise ValueError("encoder is not initialized")
-
+def _get_message_index_upto_max_tokens(system_message, chat_messages, soft_token_limit, hard_token_limit):
     num_tokens = 0
     num_tokens += 2
     num_tokens += 1
@@ -48,7 +40,7 @@ def get_message_index_upto_max_tokens(system_message, chat_messages, soft_token_
     start_index = len(chat_messages)
 
     if system_message != None:
-        num_tokens += get_message_tokens(system_message)
+        num_tokens += _get_message_tokens(system_message)
 
     if num_tokens > hard_token_limit:
         return [include_system_message, start_index]
@@ -60,7 +52,7 @@ def get_message_index_upto_max_tokens(system_message, chat_messages, soft_token_
     for start_index in range(len(chat_messages), 0, -1):
         message = chat_messages[start_index - 1]
 
-        num_tokens += get_message_tokens(message)
+        num_tokens += _get_message_tokens(message)
 
         if num_tokens <= soft_token_limit:
             num_messages += 1
@@ -77,11 +69,7 @@ def get_message_index_upto_max_tokens(system_message, chat_messages, soft_token_
 def get_messages_upto_max_tokens(ctx, system_message, chat_messages, soft_token_limit, hard_token_limit):
     ctx.log(f"getMessagesUptoMaxTokens: chatMessages.length={len(chat_messages)}, softTokenLimit={soft_token_limit}, hardTokenLimit={hard_token_limit}")
 
-    global encoder
-    if not encoder:
-        raise ValueError("encoder is not initialized")
-
-    include_system_message, start_index = get_message_index_upto_max_tokens(system_message, chat_messages, soft_token_limit, hard_token_limit)
+    include_system_message, start_index = _get_message_index_upto_max_tokens(system_message, chat_messages, soft_token_limit, hard_token_limit)
 
     result = [system_message] if include_system_message else []
 
@@ -91,17 +79,3 @@ def get_messages_upto_max_tokens(ctx, system_message, chat_messages, soft_token_
     result += chat_messages[start_index:]
 
     return result
-
-def init():
-    global encoder
-    try:
-        encoder = tiktoken.get_encoding("cl100k_base")
-    except Exception as error:
-        print('Error occurred while initializing:', error)
-
-def cleanup():
-    global encoder
-    encoder = None
-
-init()
-
